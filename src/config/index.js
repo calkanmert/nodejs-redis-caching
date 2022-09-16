37f143b3737d4887aba07b919ee2b157690cd33c
Mert Çalkan <calkanmert@outlook.com>
2022-09-16 23:40:26 +0300

    fix syntax

--- src/config/index.js
+++ src/config/index.js
@@ -5,5 +5,5 @@ dotenv.config();
 module.exports = {
   APP_PORT: process.env.APP_PORT || 4200,
   MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/redis-cache',
-  REDIS_URI: process.env.MONGO_URI || 'redis://localhost:6379',
+  REDIS_URI: process.env.REDIS_URI || 'redis://localhost:6379',
 };
